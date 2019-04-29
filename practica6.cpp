1fccf7a750e002fddb8246bbad1e8d3794a5857c
RubenZx <ruben.mondom@gmail.com>
2019-04-29 19:33:11 +0200

    1st commit

---
+++ practica6.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+// #include "../Material_practicas/alg_grafoPMC.h"
+
+using namespace std;
+
+// PROBLEMA 2. Aplicamos Floydd, cogemos la suma de los dos maximos de cada 
+// fila. Y nos quedamos con el mínimo de éste
+template <typename T>
+int diametro(const GrafoP<T>& G)
+{
+    
+}
+
+// PROBLEMA 3. Aplicamos Floydd, y vemos que ninguno de sus elementos sea inf.
+
+// Problema 4. 
+/*
+    a) Es dirigido.
+    b) Ciudades a inf.
+    c) Caminos a inf.
+    d) Dijkstra y DijkstraInv para ver como entrar y salir de la capital entre 
+       dos caminos mínimos.
+*/
+
+
+
+int main()
+{
+    return 0;
+}
