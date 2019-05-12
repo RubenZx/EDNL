2f90de12d1d7a845bd79a4e3bb13c3675cf7bdb6
RubenZx <ruben.mondom@gmail.com>
2019-05-12 18:03:14 +0200

    P7_ej2_doesnt_matchTemplate

--- practica7.cpp
+++ practica7.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <utility>
 #include <algorithm>
 #include "materialesGrafos/alg_grafoPMC.h"
 #include "materialesGrafos/alg_grafo_E-S.h"
@@ -6,7 +7,7 @@
 using namespace std;
 
 // PROBLEMA 1. Coste, origen y destino, del viaje más caro entre dos ciudades de
-// un grafo acíclico.
+// un grafo acíclico
 template <typename tCoste>
 typename GrafoP<tCoste>::arista otraVezUnGrafoSA(const GrafoP<tCoste>& G)
 {
@@ -30,21 +31,27 @@ typename GrafoP<tCoste>::arista otraVezUnGrafoSA(const GrafoP<tCoste>& G)
     return viaje;
 }
 
-// PROBLEMA 2. 
+// PROBLEMA 2. Laberinto, calcular el camino para ir de la entrada a la salida y
+// su longitud
 template <typename tCoste>
-typename GrafoP<tCoste>::tCamino laberinto(int N, 
-                                  const vector<typename GrafoP<tCoste>::arista>& paredes, 
-                                  typename GrafoP<tCoste>::vertice entrada, 
-                                  typename GrafoP<tCoste>::vertice salida)
+pair<typename GrafoP<tCoste>::tCamino , tCoste> 
+laberinto(int N, vector<typename GrafoP<tCoste>::arista> paredes, 
+          typename GrafoP<tCoste>::vertice entrada, 
+          typename GrafoP<tCoste>::vertice salida)
 {
+    typedef typename GrafoP<tCoste>::vertice vertice;
+    vertice v, w;
+    vector<vertice> P;
+    typename GrafoP<tCoste>::tCamino C;
+    pair<typename GrafoP<tCoste>::tCamino , tCoste> salidaLab;
     GrafoP<tCoste> L(N*N);
-    typename GrafoP<tCoste>::vertice v, w;
-    int ind = 0;
-
+    int ind;
     //==========================================================================
     // PREPROCESADO DE LOS DATOS DE ENTRADA (Construcción del laberinto):
     //==========================================================================
-    // Ponemos todos los caminos posibles con el mismo coste
+    // Ponemos todos los caminos posibles con el mismo coste(suponemos que cada 
+    // arista entre dos nodos tiene coste 1)
+    ind = 0;
     while(ind < N*N)
     {
         if(ind % N < N - 1) 
@@ -57,7 +64,6 @@ typename GrafoP<tCoste>::tCamino laberinto(int N,
             L[ind][ind+N] = 1;      // Abajo
         ind++;
     }
-    
     // Ponemos cada pared como aristas a INFINITO, es decir, no podremos usarla 
     // para ir de un nodo a otro
     for(auto p : paredes)
@@ -65,23 +71,36 @@ typename GrafoP<tCoste>::tCamino laberinto(int N,
         L[p.orig][p.dest] = GrafoP<tCoste>::INFINITO;
         L[p.dest][p.orig] = GrafoP<tCoste>::INFINITO;
     }
-
     //==========================================================================
     // Ya tenemos el laberinto formado, ahora tenemos que buscar el camino de 
-    // coste mínimo entre la entrada y la salida    
+    // coste mínimo entre la entrada y la salida, realizamos Dijkstra para
+    // hallar el vector P entre el nodo 'entrada' y todos los demás, calculamos
+    // el camino de coste mínimo del leberinto mediante la función camino
+    //==========================================================================
+    vector<tCoste> D = Dijkstra(L, entrada, P);     
+    tCoste cSalida = D[salida];                 // Coste mín, de salir
+    C = camino(entrada, salida, P);             // Camino más óptimo
+    salidaLab = make_pair(C, cSalida);
+    
+    return salidaLab;
 }
 
 int main()
 {
+    unsigned inf = GrafoP<unsigned>::INFINITO;
     GrafoP<unsigned> G("grafo.txt");
     GrafoP<unsigned>::arista viaje;
     int N = 3;
-    vector<GrafoP<unsigned>::arista> paredes;
-    GrafoP<unsigned>::vertice entrada, salida;
+    vector<GrafoP<unsigned>::arista> paredes{GrafoP<unsigned>::arista(0,1,inf),
+                                             GrafoP<unsigned>::arista(4,5,inf)};
+    GrafoP<unsigned>::vertice entrada{3}, salida{8};
+    pair<GrafoP<unsigned>::tCamino , unsigned> lab(laberinto(N, paredes, entrada, salida));
     
     viaje = otraVezUnGrafoSA(G);
     cout << "\n -> OTRAVEZUNGRAFOSA PROPONE COMO VIAJE..." << viaje.coste;
-    cout << endl;
-    
+    cout << "\n -> SALIDA DEL LABERINTO... ";
+    // Imprimir aqui los elementos de la lista tCamino del lab
+    cout << "\n" << endl;
+
     return 0;
 }
