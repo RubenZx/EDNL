df9d17bae24cd2060c7866c17d00292cef0c63e8
RubenZx <ruben.mondom@gmail.com>
2019-06-05 22:22:40 +0200

    generalizing

--- tools.hpp
+++ tools.hpp
@@ -18,24 +18,59 @@ void imprimirLista(const Lista<T>& lista) {
 
 // Función para crear el grafo grande con los distintos costes de los
 // transbordos entre cada nodo del grafo
+// template <typename tCoste>
+// GrafoP<tCoste> makeBigGraph(const vector<GrafoP<tCoste>>& vGrafos,
+//                             const vector<vector<tCoste>>& vCostes) {
+//     size_t n{vGrafos[0].numVert()}, m{vGrafos.size()};
+//     GrafoP<tCoste> bigGraph(n * m);
+
+//     vertice<tCoste> v, w;
+//     for (size_t i = 0; i < m; ++i) {
+//         for (v = 0; v < n; ++v) {  // n = dim. del grafo que recibimos
+//             for (w = 0; w < n; ++w) {
+//                 bigGraph[v + n * i][w + n * i] = vGrafos[i][v][w];
+//                 if (v == w) {
+//                     for (size_t j = 0; j < m; ++j) {
+//                         if (size_t iv = v + n * i, jw = w + n * j; iv != jw)
+//                             bigGraph[iv][jw] = vCostes[i][jw % n];
+//                     }
+//                 }
+//             }
+//         }
+//     }
+
+//     return bigGraph;
+// }
+
 template <typename tCoste>
 GrafoP<tCoste> makeBigGraph(const vector<GrafoP<tCoste>>& vGrafos,
                             const vector<vector<tCoste>>& vCostes) {
-    size_t n{vGrafos[0].numVert()}, m{vGrafos.size()};
-    GrafoP<tCoste> bigGraph(n * m);
+    size_t dimGraph{vGrafos[0].numVert()}, numGraphs{vGrafos.size()};
+    size_t dimBigGraph{dimGraph*numGraphs};
+    GrafoP<tCoste> bigGraph(dimBigGraph);
 
     vertice<tCoste> v, w;
-    for (size_t i = 0; i < m; ++i) {
-        for (v = 0; v < n; ++v) {  // n = dim. del grafo que recibimos
-            for (w = 0; w < n; ++w) {
-                bigGraph[v + n * i][w + n * i] = vGrafos[i][v][w];
-                if (v == w) {
-                    for (size_t j = 0; j < m; ++j) {
-                        if (size_t iv = v + n * i, jw = w + n * j; iv != jw)
-                            bigGraph[iv][jw] = vCostes[i][jw % n];
+    for (size_t i = 0; i < numGraphs; ++i) {
+        for (size_t j = 0; j < numGraphs; ++j) {
+            if(i == j){     // Rellenamos los costes de los grafos
+                for (v = 0; v < dimGraph; ++v) {  
+                    for (w = 0; w < dimGraph; ++w) {
+                        bigGraph[v + dimGraph * i][w + dimGraph * i] = vGrafos[i][v][w];
                     }
                 }
+
+            }else{
+                if(i < j){  // Rellenamos el coste del transbordo en la diag. sup.
+                    for (v = i; v < dimBigGraph; ++v) { 
+                        for (w = j; v < dimBigGraph; ++v) { 
+                            bigGraph[i][j] = vCostes[i][v];
+                        }    
+                    }
+                } // else{      // Rellenamos el coste del transbordo de la diag. inf.
+
+                // }
             }
+
         }
     }
 
