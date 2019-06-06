7b88950c3e99f07c40552a06dc5674d51710e498
RubenZx <ruben.mondom@gmail.com>
2019-06-06 20:32:49 +0200

    added makeBigGraph for different dimension, not finished

--- tools.hpp
+++ tools.hpp
@@ -11,6 +11,9 @@ using std::tuple;
 template <typename tCoste>
 using vertice = typename GrafoP<tCoste>::vertice;
 
+template <typename tCoste>
+using arista = typename GrafoP<tCoste>::arista;
+
 template <typename T>
 tuple<size_t, T> minIndex(const vector<T>& v) {
     auto ind = min_element(v.begin(), v.end());
@@ -120,4 +123,41 @@ GrafoP<tCoste> makeBigGraph(const vector<GrafoP<tCoste>>& vGrafos, tCoste cst) {
     }
 
     return bigGraph;
+}
+
+// Función para crear el grafo grande a partir de grafos de distinto tamaños
+template <typename tCoste>
+GrafoP<tCoste> makeBGdifN(const vector<GrafoP<tCoste>>& vGrafos,
+                          const vector<arista<tCoste>>& vCost) {
+    size_t nGraphs{vGrafos.size()}, tot{0};
+    vector<size_t> vTam(nGraphs, 0);
+
+    // Guardamos el tamaño de cada grafo en un vector y el total en la var. tot
+    for (size_t ind = 0; ind < nGraphs; ++ind) {
+        vTam[ind] = vGrafos[ind].numVert();
+        tot += vTam[ind];
+    }
+
+    GrafoP<tCoste> bG(tot);
+    vector<arista<tCoste>> aristasI;
+    vertice<tCoste> v, w, x, y;
+    size_t sum = 0, tamAct, indbG;
+    // Bucle hasta el número de grafos que tengamos
+    for (size_t i = 0; i < nGraphs; ++i) {
+        tamAct = vTam[i];      // Tomamos la dim. del grafo i-ésimo
+        sum += tamAct;         // Acumulamos las dimensiones
+        indbG = sum - tamAct;  // Contador para indexar el grafo grande
+        for (v = 0, x = indbG; v < tamAct; ++v, ++x) {
+            for (w = 0, y = indbG; w < tamAct; ++w, ++y) {
+                bG[x][y] = vGrafos[i][v][w];
+            }
+        }
+    }
+
+    // for (size_t j = 0; j < vCost.size(); ++j) {
+    //     bG[vCost[j].orig][vCost[j].dest] = bG[vCost[j].dest][vCost[j].orig] =
+    //         vCost[j].coste;
+    // }
+
+    return bG;
 }
