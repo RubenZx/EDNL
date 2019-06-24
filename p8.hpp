5c562b15c9ee2b00abb7dcf129a3c121a44a41d7
RubenZx <ruben.mondom@gmail.com>
2019-06-24 17:28:12 +0200

    ex2_p8 fixed

--- p8.hpp
+++ p8.hpp
@@ -62,81 +62,62 @@ tuple<vector<vector<vertice<size_t>>>, matriz<double>> Tombuctu(
 
 // Problema 2:
 // Encontrar como conectar las ciudades de las islas mediante aeropuertos
-// minimizando el coste entre todas las ciudades
-vector<arista<double>> Tombuctu2(const vector<coordCart>& ciudades,
-                                 const Grafo& carreteras) {
-    int nCiud = ciudades.size();
-    Particion P{nCiud};
+// minimizando el coste entre los aeropuertos de las islas
+struct ciudad {
+    double x, y;
+    ciudad() = default;
+    double distEuclid(const ciudad& c) const {
+        return sqrt(pow(x - c.x, 2) + pow(y - c.y, 2));
+    }
+};
+
+vector<typename GrafoP<double>::arista> Tombuctu2(
+    const vector<ciudad>& ciudades, const Grafo& conexiones) {
+    size_t nIslas = nCiud = ciudades.size();
     GrafoP<double> G(nCiud);
+    Particion P(nCiud);
 
-    // Ponemos los costes de ir de todas las ciudades a todas las demas
-    int repre1 = 0, repre2 = 0;
-    for (size_t v = 0; v < nCiud; ++v) {
-        repre1 = P.encontrar(v);
-        for (size_t w = v; w < nCiud; ++w) {
-            if (carreteras[v][w]) {
-                repre2 = P.encontrar(w);
-                P.unir(repre1, repre2);
+    int r1, r2;
+    typename GrafoP<double>::vertice v, w;
+    for (v = 0; v < nCiud; ++v) {
+        r1 = P.encontrar(v);
+        for (w = v; w < nCiud; ++w) {
+            r2 = P.encontrar(w);
+            if (conexiones[v][w] && r1 != r2) P.unir(r1, r2);
+        }
+    }
+
+    // Cada key será la isla i, y el vector contendrá las ciudades de la isla i
+    map<int, vector<typename GrafoP<double>::vertice>> islas;
+    for (size_t i = 0; i < nCiud; ++i) {
+        islas[P.encontrar(i)].push_back(i);
+    }
+
+    vector<typename GrafoP<double>::arista> aerolineas;
+    vector<typename GrafoP<double>::vertice> islaAct, isla2;
+    double costeAct, costeMin{GrafoP<double>::INFINITO};
+    typename GrafoP<double>::arista aerolineaAct;
+    for (auto isla_i = islas.begin(); isla_i != islas.end(); ++isla_i) {
+        islaAct = isla_i->second;
+        for (auto islaSig = next(isla_i); islaSig != islas.end(); ++islaSig) {
+            isla2 = islaSig->second;
+            for (size_t i = 0; i < islaAct.size(); ++i) {
+                for (size_t j = 0; j < isla2.size(); ++j) {
+                    costeAct =
+                        ciudades[islaAct[i]].distEuclid(ciudades[isla2[j]]);
+                    if (costeAct < costeMin) {
+                        aerolineaAct.orig = islaAct[i];
+                        aerolineaAct.dest = isla2[j];
+                        aerolineaAct.coste = costeAct;
+                    }
+                }
             }
-            G[v][w] = G[w][v] = ciudades[v].distEuclid(ciudades[w]);
+            aerolineas.push_back(aerolineaAct);
+            costeMin = GrafoP<double>::INFINITO;
         }
     }
 
-    // Guardamos las islas en un vector, de vector en los cuales cada posición
-    // corresponde a las ciudades de cada isla
-    repre1 = P.encontrar(0);
-    vector<vector<vertice<size_t>>> islas(1, vector<size_t>(1, 0));
-    size_t nIslas = 0;
-    for (size_t i = 1; i < nCiud; ++i) {
-        if (repre1 == P.encontrar(i))
-            islas[nIslas].push_back(i);
-        else {
-            ++nIslas;
-            repre1 = P.encontrar(i);
-            islas.push_back(vector<size_t>(1, i));
-        }
-    }
-
-    // Calculamos Floyd para buscar lamejor ciudad de cada isla para poner ahhí
-    // el aeropuerto
-    matriz<vertice<double>> mP;
-    matriz<double> floyd{Floyd(G, mP)};
-
-    double costeAct, costesMin{GrafoP<double>::INFINITO};
-    size_t ant = 0;
-    // Vector de vectores de representantes por cada islas
-    vector<vertice<double>> repre(nIslas);
-    for (size_t i = 0; i < nIslas; ++i) {  // Bucle para recorrer cada isla
-        size_t nCiud = islas[i].size();
-        for (size_t j = ant; j < ant + nCiud; ++j) {
-            // Hacemos la suma de cada fila y vemos si su coste es menor,
-            // para asi elegir el representante de cada isla
-            costeAct = accumulate(
-                floyd[j].begin(), floyd[j].end(), 0, [](double a, double b) {
-                    return ((b != GrafoP<double>::INFINITO) ? a + b : a);
-                });
-
-            if (costeAct < costesMin) {
-                costesMin = costeAct;
-                repre[i] = j;
-            }
-        }
-        costesMin = GrafoP<double>::INFINITO;
-        ant += islas[i].size();
-    }
-
-    // Ahora rellenamos las aerolineas
-    vector<arista<double>> aerolinea;
-
-    for (size_t i = 0; i < nIslas; ++i) {
-        for (size_t j = i + 1; j < nIslas; ++j) {
-            size_t a = repre[i], b = repre[j];
-            aerolinea.push_back(
-                arista<double>{a, b, ciudades[a].distEuclid(ciudades[b])});
-        }
-    }
-
-    return aerolinea;
+    return aerolineas;
 }
 
 // Problema 4:
@@ -288,5 +269,4 @@ double reconstruirZuelandia(const vector<coordCart>& Fobos,
     // ambas islas
     matriz<vertice<double>> mP;
     matriz<double> floyd{Floyd(bG, mP)};
-
 }
